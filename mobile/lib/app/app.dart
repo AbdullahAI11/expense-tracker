2d9cfe07b4dde74e833a4e3a90a5d1dde72d77ef
Abdullah Youns <10abdullahai10@gmail.com>
2026-08-26 17:06:33 +0300

    Add system light and dark theme support

--- mobile/lib/app/app.dart
+++ mobile/lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/app/theme/app_theme.dart';
 import 'package:expense_tracker/features/expenses/screens/home_screen.dart';
 
 class ExpenseTrackerApp extends StatelessWidget {
@@ -7,6 +8,11 @@ class ExpenseTrackerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return MaterialApp(
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
+      home: const HomeScreen(),
+    );
   }
 }
